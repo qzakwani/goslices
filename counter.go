54c22c08f9f64013314dbfbf962771caf9ef15f0
Alqasim Alzakwani <alqasim.zakwani@rihal.om>
2023-10-04 20:07:25 +0400

    updated counter types

--- counter.go
+++ counter.go
@@ -1,16 +1,16 @@
 package goslices
 
-type Counter[K comparable] struct {
+type Counter[V comparable] struct {
 	Total   int
-	Set     map[K]int
-	Order   []K
+	Set     map[V]int
+	// Order   []K
 	Ordered bool
 }
 
 // Initialize a Counter[K] with a slice of K.
-func NewCounter[K comparable](s *[]K) *Counter[K] {
-	m := make(map[K]int)
-	for _, v := range *s {
+func NewCounter[T ~[]V, V comparable](s T) *Counter[V] {
+	m := make(map[V]int)
+	for _, v := range s {
 		_, ok := m[v]
 		if ok {
 			m[v]++
@@ -18,27 +18,27 @@ func NewCounter[K comparable](s *[]K) *Counter[K] {
 			m[v] = 1
 		}
 	}
-	return &Counter[K]{
-		Total: len(*s),
+	return &Counter[V]{
+		Total: len(s),
 		Set:   m,
 	}
 }
 
 // Get the total number of element k in the Counter[K].
-func (c *Counter[K]) Count(k K) int {
-	v, ok := c.Set[k]
+func (c *Counter[V]) Count(v V) int {
+	i, ok := c.Set[v]
 	if ok {
-		return v
+		return i
 	}
 	return 0
 }
 
 // Delete element k from the Counter[K].
-func (c *Counter[K]) Delete(k K) {
-	_, ok := c.Set[k]
+func (c *Counter[V]) Delete(v V) {
+	_, ok := c.Set[v]
 	if ok {
-		c.Total = c.Total - c.Set[k]
-		delete(c.Set, k)
+		c.Total = c.Total - c.Set[v]
+		delete(c.Set, v)
 	}
 }
 
